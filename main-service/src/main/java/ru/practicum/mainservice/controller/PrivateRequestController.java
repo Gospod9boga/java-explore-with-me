1297c600b3c68ba8b6c4312baf81a0b1b20b7151
Влад <vlad@MacBook-Pro-Vlad.local>
2026-02-11 23:33:24 +0300

    FIX: add missing PrivateRequestController

---
+++ main-service/src/main/java/ru/practicum/mainservice/controller/PrivateRequestController.java
@@ -0,0 +1,40 @@
+package ru.practicum.mainservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.mainservice.dto.response.ParticipationRequestDto;
+import ru.practicum.mainservice.service.RequestService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users/{userId}/requests")
+public class PrivateRequestController {
+
+    private final RequestService requestService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ParticipationRequestDto createRequest(@PathVariable Long userId,
+                                                 @RequestParam Long eventId) {
+        log.info("POST /users/{}/requests?eventId={}", userId, eventId);
+        return requestService.createRequest(userId, eventId);
+    }
+
+    @GetMapping
+    public List<ParticipationRequestDto> getUserRequests(@PathVariable Long userId) {
+        log.info("GET /users/{}/requests", userId);
+        return requestService.getUserRequests(userId);
+    }
+
+    @PatchMapping("/{requestId}/cancel")
+    public ParticipationRequestDto cancelRequest(@PathVariable Long userId,
+                                                 @PathVariable Long requestId) {
+        log.info("PATCH /users/{}/requests/{}/cancel", userId, requestId);
+        return requestService.cancelRequest(userId, requestId);
+    }
+}
