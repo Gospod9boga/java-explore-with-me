31711887b3b20a875de59d337ded3e55de9ff8ee
Влад <vlad@MacBook-Pro-Vlad.local>
2026-02-12 04:58:44 +0300

    удален лишний импорт

--- main-service/src/main/java/ru/practicum/mainservice/dto/request/NewEventDto.java
+++ main-service/src/main/java/ru/practicum/mainservice/dto/request/NewEventDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 import ru.practicum.mainservice.dto.LocationDto;
-import ru.practicum.mainservice.model.entity.Event;
 
 import java.time.LocalDateTime;
 
